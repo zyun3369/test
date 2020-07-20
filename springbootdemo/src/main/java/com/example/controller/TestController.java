62e571af475042c79cd691aab9849cacbf392981
zyun3369 <13361653369@163.com>
2020-07-20 20:23:34 +0800

    demo

--- springbootdemo/src/main/java/com/example/controller/TestController.java
+++ springbootdemo/src/main/java/com/example/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.example.controller;
 
+import com.example.entity.Student;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +9,13 @@ public class TestController {
     @RequestMapping("/test")
     public String test(){
         return "hello springboot";
+    }
+
+    @RequestMapping("/testobj")
+    public Student testobj(){
+        Student student = new Student();
+        student.setAge(11);
+        student.setName("gg");
+        return student;
     }
 }
